9daf6f385e03b09debfb87e978fcb8b5dfaca77a
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-22 23:12:50 +0300

    Appointment GetAll completed
    Appointment GetAll completed

---
+++ eAppointmentServer/eAppointmentServer.Application/Features/Appointments/GetAllAppointmentsByDoctorId/GetAllAppointmentsByDoctorIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Appointments.GetAllAppointmentsByDoctorId;
+public sealed record GetAllAppointmentsByDoctorIdQuery(
+    Guid DoctorId) : IRequest<Result<List<GetAllAppointmentsByDoctorIdQueryResponse>>>;
