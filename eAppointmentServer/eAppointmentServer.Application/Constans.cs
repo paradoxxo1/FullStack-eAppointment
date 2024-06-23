426ee141646eb76a7672b21de706c82579f3d5a7
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-23 16:47:51 +0300

    Creating a static role list and synchronizing it with the database done
    Creating a static role list and synchronizing it with the database done

---
+++ eAppointmentServer/eAppointmentServer.Application/Constans.cs
@@ -0,0 +1,21 @@
+﻿using eAppointmentServer.Domain.Entities;
+
+namespace eAppointmentServer.Application;
+public static class Constans
+{
+    public static List<AppRole> GetRoles()
+    {
+        List<string> roles = new()
+        {
+            "Admin",
+            "Doctor",
+            "Personel",
+            "Patient"
+        };
+
+        return roles.Select(s => new AppRole() { Name = s }).ToList();
+    }
+}
+
+
+
---
+++ eAppointmentServer/eAppointmentServer.Application/Features/Roles/RoleSync/RoleSyncCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Roles.RoleSync;
+public sealed record RoleSyncCommand() : IRequest<Result<string>>;
