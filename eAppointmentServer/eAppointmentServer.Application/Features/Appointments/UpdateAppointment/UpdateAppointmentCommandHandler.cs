5d165e6ef5ea63197e3f575ece53332c35158110
Mahmut DEMİRKIRAN <124463263+paradoxxo1@users.noreply.github.com>
2024-06-23 15:04:36 +0300

    Appointment Delete and Update Done
    Appointment Delete and Update Done

---
+++ eAppointmentServer/eAppointmentServer.Application/Features/Appointments/UpdateAppointment/UpdateAppointmentCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using eAppointmentServer.Domain.Entities;
+using eAppointmentServer.Domain.Repositories;
+using GenericRepository;
+using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Appointments.UpdateAppointment;
+
+internal sealed class UpdateAppointmentCommandHandler(
+    IAppointmentRepository appointmentRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<UpdateAppointmentCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UpdateAppointmentCommand request, CancellationToken cancellationToken)
+    {
+        Appointment appointment =
+             await appointmentRepository
+             .GetByExpressionWithTrackingAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (appointment is null)
+        {
+            return Result<string>.Failure("Appointment is not Found");
+        }
+        appointment.StartDate = Convert.ToDateTime(request.StartDate);
+        appointment.EndDate = Convert.ToDateTime(request.EndDate);
+
+        //appointmentRepository.Update(appointment); // GetByExpressionWithTrackingAsync bu method tracking yaptığı için bu alan yazılmıyor.
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Appointment update is successful...";
+
+    }
+}
--- eAppointmentServer/eAppointmentServer.WebAPI/Controllers/AppointmentsController.cs
+++ eAppointmentServer/eAppointmentServer.WebAPI/Controllers/AppointmentsController.cs
@@ -1,7 +1,9 @@
 ﻿using eAppointmentServer.Application.Features.Appointments.CreateAppointment;
+using eAppointmentServer.Application.Features.Appointments.DeleteAppointmentById;
 using eAppointmentServer.Application.Features.Appointments.GetAllAppointmentsByDoctorId;
 using eAppointmentServer.Application.Features.Appointments.GetAllDoctorByDepartment;
 using eAppointmentServer.Application.Features.Appointments.GetPatientByIdentityNumber;
+using eAppointmentServer.Application.Features.Appointments.UpdateAppointment;
 using eAppointmentServer.WebAPI.Abstractions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -37,6 +39,20 @@ public sealed class AppointmentsController : ApiController
 
     [HttpPost]
     public async Task<IActionResult> Create(CreateAppointmentCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteById(DeleteAppointmentByIdCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(UpdateAppointmentCommand request, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(request, cancellationToken);
         return StatusCode(response.StatusCode, response);
